7cf24bbf0348568681ae91f9fa6d3ae51621e9be
Marcus Pöhls <mp@seda.digital>
2021-05-17 10:48:42 +0200

    test console logger

---
+++ test/console-logger.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const Sinon = require('sinon')
+const { test } = require('tap')
+const { ConsoleOutput } = require('../dist')
+
+test('Console Logger', async t => {
+  test('logs info message to console', async t => {
+    const consoleLogSpy = Sinon.stub(console, 'log').returns()
+    const consoleErrorSpy = Sinon.stub(console, 'error').returns()
+
+    const output = new ConsoleOutput()
+
+    output.info('Hello Supercharge')
+
+    t.ok(consoleLogSpy.called)
+    t.ok(consoleErrorSpy.notCalled)
+
+    consoleLogSpy.restore()
+    consoleErrorSpy.restore()
+  })
+
+  test('logs error message to console', async t => {
+    const consoleLogSpy = Sinon.stub(console, 'log').returns()
+    const consoleErrorSpy = Sinon.stub(console, 'error').returns()
+
+    const output = new ConsoleOutput()
+
+    output.error('Error!')
+
+    t.ok(consoleErrorSpy.called)
+    t.ok(consoleLogSpy.notCalled)
+
+    consoleLogSpy.restore()
+    consoleErrorSpy.restore()
+  })
+})
